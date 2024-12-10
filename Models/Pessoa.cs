322b2fb89d5e448efb37a1ff89c828b0e0229334
Mateus Perpetuo <mateus_perpetuo@outlook.com>
2024-12-09 21:05:02 -0300

    Validações da prop Idade

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -3,24 +3,33 @@ namespace ESSENCIALS.Models;
 public class Pessoa
 {
     private string _nome;
-    public string Nome 
-    { 
-        get
-        {
-            return _nome.ToUpper();  
-        } 
+    private int _idade;
+    public string Nome
+    {
+        get => _nome.ToUpper();
 
         set
         {
-            if ( value == "")
+            if (value == "")
             {
                 throw new ArgumentException("O nome não pode ser vazio");
             }
             _nome = value;
-        } 
+        }
+
     }
 
-    public int Idade { get; set; }
+    public int Idade
+    {   get => _idade;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("A idade não pode ser menor que zero");
+            }
+            _idade = value;
+        } 
+    }
 
     public void Apresentar()
     {
